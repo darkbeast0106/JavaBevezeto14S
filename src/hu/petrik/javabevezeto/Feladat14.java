90aa30ee2e771b6c2d438466becc338fc84a91d8
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-15 18:19:11 +0200

    14. Feladat

--- src/hu/petrik/javabevezeto/Feladat14.java
+++ src/hu/petrik/javabevezeto/Feladat14.java
@@ -18,6 +18,44 @@ public class Feladat14 {
             System.out.println("\tf - Kilépni");
             System.out.print("Kérem adja meg az elvégzendő művelet betűjelét: ");
             menuPont = sc.nextLine().toLowerCase();
+            switch (menuPont) {
+                case "a":
+                    System.out.println(szoveg.toUpperCase());
+                    break;
+                case "b":
+                    System.out.println(szoveg.toLowerCase());
+                    break;
+                case "c":
+                    System.out.printf("A szöveg hossza %d karakter\n", szoveg.length());
+                    break;
+                case "d":
+                    System.out.print("Kérem adja meg a másik szöveg: ");
+                    String masikSzoveg = sc.nextLine();
+                    int osszehasonlitasEredmenye = szoveg.compareTo(masikSzoveg);
+                    if (osszehasonlitasEredmenye == 0) {
+                        System.out.println("A két szöveg megegyezik");
+                    } else if (osszehasonlitasEredmenye < 0) {
+                        System.out.println("Az első szöveg előrébb van az ABC-ben");
+                    } else {
+                        System.out.println("A második szöveg előrébb van az ABC-ben");
+                    }
+                    break;
+                case "e":
+                    System.out.print("Kérem adja meg az első kiiratandó karakter sorszámát: ");
+                    int a = sc.nextInt();
+                    sc.nextLine();
+                    System.out.print("Kérem adja meg az utolsó kiiratandó karakter sorszámát: ");
+                    int b = sc.nextInt();
+                    sc.nextLine();
+                    System.out.println(szoveg.substring(a - 1, b));
+                    break;
+                case "f":
+                    System.out.println("Viszlát!");
+                    break;
+                default:
+                    System.out.println("Ilyen menüpont nem létezik");
+                    break;
+            }
         }
     }
 }
